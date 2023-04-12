588b9b5824719ae41eca777e011bf9c8b2754b27
Franperceb <42518428+Franperceb@users.noreply.github.com>
2023-04-12 12:06:58 -0600

    removing innecesary files

--- backend/src/routes/private.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getPrivateRoute } from '../controllers/private.js';
-//import { protect } from '../middleware/auth.ts';
-
-const router = Router();
-
-//router.route('/').get(protect, getPrivateRoute);
-
-export default router;

0dc9aae4741aeeac07e6d68f95bed574ea2176c9
Franperceb <42518428+Franperceb@users.noreply.github.com>
2023-04-14 12:25:02 -0600

    migrating UI to nextJS

---
+++ ui/next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    appDir: true,
+  },
+  env: {
+    API_URL: 'prueba',
+  },
+};
+
+module.exports = nextConfig;
